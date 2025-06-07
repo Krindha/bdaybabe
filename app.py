a36479243547a204f1fc6f656eb44a27b95035fb
Krindha <86869866+Krindha@users.noreply.github.com>
2025-06-07 21:06:15 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template("index.html")
+
+@app.route('/message')
+def message():
+    return render_template("message.html")
+
+@app.route('/surprise')
+def surprise():
+    return render_template("surprise.html")
+
+if __name__ == '__main__':
+    app.run(debug=True)
