cf8cffa2d335b32bd65366c42f70522ab26c4d45
Krindha <86869866+Krindha@users.noreply.github.com>
2025-06-07 21:30:44 +0530

    app.py

--- app.py
+++ app.py
@@ -1,21 +1,12 @@
 from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def home():
     return render_template("index.html")
 
-@app.route('/message')
-def message():
-    return render_template("message.html")
-
-@app.route('/surprise')
-def surprise():
-    return render_template("surprise.html")
-
-if __name__ == '__main__':
-    import os
-
-port = int(os.environ.get("PORT", 5000))
-app.run(host='0.0.0.0', port=port)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
